d8ed1a03cc85d4dc2aaeff5e8dc24556db32e400
Nathan Brannon <nbrannon@uci.edu>
2014-11-09 16:42:42 -0800

    Added back end folder and waypoint handling class

---
+++ Back End/waypoint handling/waypoint.cpp
@@ -0,0 +1,15 @@
+#include "waypoint.hpp"
+
+mapWayPoint()
+	:locLat{0}, locLong({0}, locBear{0}
+{
+
+}
+
+mapWayPoint(double initialLocLat, double initialLocLong, double initialLocBear)
+	:locLat{initialLocLat}, locLong{initialLocLong}, locBear{initialLocBear}
+{
+
+}
+
+void setLocLong
